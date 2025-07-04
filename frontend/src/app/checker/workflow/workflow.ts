641d001db34a8560bd45b8526d3506ac210370d6
tanushreemadaan <tanushreemadaan1553@gmail.com>
2025-07-04 16:07:32 +0530

    Added workflow page; arrows left

---
+++ frontend/src/app/checker/workflow/workflow.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-workflow',
+  imports: [RouterModule],
+  templateUrl: './workflow.html',
+  styleUrl: './workflow.css'
+})
+export class Workflow {
+
+}
